b7e409e2d755c896902a46e6410b844c6a0253f4
mikemaksim <mikemaksimenko@gmail.com>
2022-08-22 14:46:08 +0300

    Checking login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -18,6 +19,14 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+    
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        
         
     def is_not_element_present(self, how, what, timeout=4):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+
 from selenium.webdriver.common.by import By
 
+"""
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -58,3 +61,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     
     # Проверяем, что нет сообщения об успехе с помощью is_disappeared
     page.success_message_disappeared()
+ """
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
